6634cd3fe8ea0a6c24fab5438e0f81c212c95fd2
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2023-06-28 11:15:27 +0200

    Improve testing
    Test all final jet parameters, to a defined tolerance using
    the IsApprox package
    Test both N2Plain and N2Tiled

--- test/jetrecotest.jl
+++ test/jetrecotest.jl
@@ -3,6 +3,8 @@
 using JetReconstruction
 using Test
 using JSON
+using IsApprox
+using IsApprox: iszero
 
 """Read JSON file with fastjet jets in it"""
 function read_fastjet_outputs(;fname="test/data/jet_collections_fastjet.json")
@@ -28,20 +30,40 @@ function main()
     fastjet_jets = read_fastjet_outputs()
     sort_jets!(fastjet_jets)
 
+    # Test each stratgy...
+    do_jet_test(N2Plain, fastjet_jets)
+    do_jet_test(N2Tiled, fastjet_jets)
+
+end
+
+function do_jet_test(strategy::JetRecoStrategy, fastjet_jets;
+    ptmin::Float64 = 5.0,
+	distance::Float64 = 0.4,
+	power::Integer = -1)
+
+	# Strategy
+	if (strategy == N2Plain)
+		jet_reconstruction = sequential_jet_reconstruct
+        strategy_name = "N2Plain"
+	elseif (strategy == N2Tiled)
+		jet_reconstruction = tiled_jet_reconstruct
+        strategy_name = "N2Tiled"
+	else
+		throw(ErrorException("Strategy not yet implemented"))
+	end
+
     # Now run our jet reconstruction...
-    jet_reconstruction = sequential_jet_reconstruct
     events::Vector{Vector{PseudoJet}} = read_final_state_particles("test/data/events.hepmc3")
     event_vector = pseudojets2vectors(events)
     jet_collection = FinalJets[]
     for (ievt, event) in enumerate(event_vector)
-        finaljets, _ = jet_reconstruction(event, R=0.4, p=-1)
-        fj = final_jets(finaljets, 5.0)
+        finaljets, _ = jet_reconstruction(event, R=distance, p=power)
+        fj = final_jets(finaljets, ptmin)
         sort_jets!(fj)
-        # println(fj)
         push!(jet_collection, FinalJets(ievt, fj))
     end
 
-    @testset verbose = true "Jet Reconstruction" begin
+    @testset "Jet Reconstruction, $strategy_name" begin
         # Test each event in turn...
         for (ievt, event) in enumerate(jet_collection)
             @testset "Event $(ievt)" begin
@@ -49,10 +71,11 @@ function main()
                 # Test each jet in turn
                 for (ijet, jet) in enumerate(event.jets)
                     if ijet <= size(fastjet_jets[ievt]["jets"])[1]
-                        # Approximat
-                        # @test jet.rap ≈ fastjet_jets[ievt]["jets"][ijet]["rap"]
-                        # @test jet.phi ≈ fastjet_jets[ievt]["jets"][ijet]["phi"]
-                        @test jet.pt ≈ fastjet_jets[ievt]["jets"][ijet]["pt"]
+                        # Approximate test - note that ≈ itself is just too strict here
+                        # There's no a≈b here, so we test the difference is ≈0
+                        @test iszero(jet.rap - fastjet_jets[ievt]["jets"][ijet]["rap"], Approx(atol=1e-7))
+                        @test iszero(jet.phi - fastjet_jets[ievt]["jets"][ijet]["phi"], Approx(atol=1e-7))
+                        @test iszero(jet.pt - fastjet_jets[ievt]["jets"][ijet]["pt"], Approx(atol=1e-7))
                     end
                 end
             end
